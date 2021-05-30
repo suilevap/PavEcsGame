95a1c74d50f84f15d041ba011bcdfb1b1e469a8c
suilevap <suilevap@gmail.com>
2021-05-30 15:12:18 +0100

    Group render command by main color to minimize color changes

--- PavEcsLiteGame/Systems/Renders/ConsoleRenderSystem.cs
+++ PavEcsLiteGame/Systems/Renders/ConsoleRenderSystem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Leopotam.EcsLite;
 using PavEcsGame.Components;
 using PavEcsGame.Components.Events;
@@ -12,23 +13,80 @@ namespace PavEcsGame.Systems.Renders
     {
 
         private readonly EcsFilterSpec<EcsSpec<RenderItemCommand>, EcsSpec, EcsSpec> _renderCommandSpec;
-
+        private List<EcsUnsafeEntity>[] _groupedbyColor = new List<EcsUnsafeEntity>[16];
         public ConsoleRenderSystem(EcsUniverse universe)
         {
             universe
                 .Build(ref _renderCommandSpec);
+
+            for (int i = 0; i < _groupedbyColor.Length; i++)
+            {
+                _groupedbyColor[i] = new List<EcsUnsafeEntity>(128);
+            }
         }
 
         public void Init(EcsSystems systems)
         {
             Console.CursorVisible = false;
         }
-
         public void Run(EcsSystems systems)
+        {
+            RenderGroupedByMainColor();
+            //RenderSimple();
+        }
+        private void RenderGroupedByMainColor()
         {
 
             var commandPool = _renderCommandSpec.Include.Pool1;
-            foreach (var ent in _renderCommandSpec.Filter)
+            foreach (EcsUnsafeEntity ent in _renderCommandSpec.Filter)
+            {
+                var color = commandPool.Get(ent).Symbol.MainColor;
+                _groupedbyColor[(int)color].Add(ent);
+            }
+
+            for (int i = 0; i < _groupedbyColor.Length; i++)
+            {
+                var list = _groupedbyColor[i];
+                if (list.Count != 0)
+                {
+                    ConsoleColor mainColor = (ConsoleColor)i;
+                    Console.ForegroundColor = mainColor;
+
+                    foreach (EcsUnsafeEntity ent in list)
+                    {
+                        RenderItemWithoutColor(in commandPool.Get(ent));
+                        commandPool.Del(ent);
+                    }
+                    list.Clear();
+                }
+            }
+            static void RenderItemWithoutColor(in RenderItemCommand item)
+            {
+                if (Console.CursorLeft != item.Position.Value.X || Console.CursorTop != item.Position.Value.Y)
+                {
+                    Console.SetCursorPosition(item.Position.Value.X, item.Position.Value.Y);
+                }
+                if (Console.BackgroundColor != item.BackgroundColor)
+                {
+                    Console.BackgroundColor = item.BackgroundColor;
+                }
+                if (item.Symbol.Value != default)
+                {
+                    Console.Write(item.Symbol.Value);
+                }
+                else
+                {
+                    //Console.ResetColor();
+                    Console.Write(SymbolComponent.Empty.Value);
+                }
+            }
+        }
+
+        private void RenderSimple()
+        {
+
+            var commandPool = _renderCommandSpec.Include.Pool1;
+            foreach (EcsUnsafeEntity ent in _renderCommandSpec.Filter)
             {
                 RenderItem(in commandPool.Get(ent));
                 commandPool.Del(ent);
